1cdd56d82d4ab86273371f01f0b895297f912798
Lucas Baez Miranda <lucasbaezmiranda@gmail.com>
2022-12-31 04:48:38 -0300

    Agregada funcion load() que lee un url y lo manda al console.log(). Falta asignarlo a un objeto.

--- 5 - Pokedex Project/pokedex-v1/src/CSV_parser.js
+++ 5 - Pokedex Project/pokedex-v1/src/CSV_parser.js
@@ -1,16 +1,21 @@
 import React, {Component} from 'react';
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
+async function load(in_url) {
+    let obj = await (await fetch(in_url)).json();
+    console.log(obj);
+}
+
+let url = 'https://pokeapi.co/api/v2/pokemon/ditto';
+load(url);
+
 class CSV_parser extends Component {
     render(){
         const ditto_link = "https://pokeapi.co/api/v2/pokemon/ditto";
         let ditto_data = d3.json(ditto_link);
 
-        const url = ditto_link;
-        fetch(url)
-        .then(res => res.json())
-        .then(out => console.log(out))
-        .catch(err => { throw err });
+        let url = ditto_link;
+
                 return(
                     <div>
                         <p>Hola - CSV parser</p>
